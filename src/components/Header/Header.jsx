c9edd977e87fad9b046dabbb43b668242030b324
fatimasny <sinayi.fatima@gmail.com>
2024-07-30 09:07:15 -0700

    check lovely teacher

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoMoonOutline } from "react-icons/io5";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 function Header() {
+  const [scrollHeader, setScrollHeader] = useState(false);
+
+  useEffect(() => {
+    const handlerScroll = () => {
+      let positionScroll = Math.round(window.scrollY);
+      if (positionScroll >= 100) {
+        setScrollHeader(true);
+      } else {
+        setScrollHeader(false);
+      }
+    };
+    window.addEventListener("scroll", handlerScroll);
+    return () => {
+      window.removeEventListener("scroll", handlerScroll);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-row justify-between  pl-10 pr-10 pt-6 sticky top-0 w-full ">
+    <header
+      className={`flex flex-row justify-between items-center  w-full    pl-10 pr-10 pt-6 ${
+        scrollHeader && "sticky top-0"
+      }  `}
+    >
       <div className="flex flex-row justify-between items-center ">
-        <h2 className="text-[1.8rem] font-bold">developer</h2>
+        <h2 className="text-[1.8rem] font-bold uppercase font-['Brands'] ">
+          developer
+        </h2>
         <span className="inline-block w-2 h-2 border-solid border-2 border-[#00ffb3] rounded-full ml-2 mt-2 "></span>
       </div>
       <div className="">
@@ -17,7 +40,7 @@ function Header() {
           <HiOutlineMenuAlt4 fontSize="2.1rem" />
         </button>
       </div>
-    </div>
+    </header>
   );
 }
 
