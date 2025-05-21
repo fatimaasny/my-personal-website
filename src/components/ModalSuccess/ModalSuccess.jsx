c64c4b4553c39bc5a2d56db36a0356fab0688938
fatimasny <sinayi.fatima@gmail.com>
2025-05-21 13:21:31 -0700

    ok modal

--- src/components/ModalSuccess/ModalSuccess.jsx
+++ src/components/ModalSuccess/ModalSuccess.jsx
@@ -4,10 +4,10 @@ function ModalSuccess({ dialogRef }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="flex items-center justify-center md:w-[35%]   mx-auto border-2 border-[#000] border-solid rounded-[10px] w-[50%] h-[10%] "
+      className=" md:w-[30%] mx-auto border-2 border-[#29a385] border-solid rounded-[10px] w-[50%] h-[8%] "
     >
-      <p className="text-[#29a587] text-[0.8rem] md:text-[1rem] ">
-        اطلاعات با موفقیت ثبت گردید.
+      <p className="text-[#29a385] text-[0.8rem] md:text-[1rem] flex pt-4  pl-2 ">
+        Your message was sent successfully.
       </p>
     </dialog>,
     document.getElementById("modal")
